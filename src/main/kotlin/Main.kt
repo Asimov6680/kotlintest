ad89d74e5bbcd110fbfcb07836a5a1f1269cce34
asimov6680 <1312158363@qq.com>
2023-11-25 15:26:55 +0800

    基本的面向对象学习

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,31 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main() {
+    val studentA = Student("安学民",18)
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    val petParse = if (studentA.isAdult) "nice to meet you!" else "yo~"
+
+    studentA.sayHello(petParse)
+
+    println("i like ${studentA.hobby()}")
+}
+
+open class Person(val name: String, var age: Int){
+
+    /**
+     * 和大家做自我介绍吧，带上你的口头禅！
+     */
+    fun sayHello(petParse :String){
+        println("hello, my name is ${name}, $petParse")
+    }
+
+    /**
+     * 兴趣爱好是啥?
+     */
+    open fun hobby() :String = "no"
+}
+
+class Student(name: String, age: Int) : Person(name, age) {
+    val isAdult :Boolean
+        get() = age >= 18
+
+    override fun hobby() :String = "football"
 }
