07e83002c1571b5edba16fa1eafe0677a7faee37
asimov6680 <1312158363@qq.com>
2023-11-26 00:10:04 +0800

    简易计算器

--- src/main/kotlin/test/Main.kt
+++ src/main/kotlin/test/Main.kt
@@ -1,3 +1,5 @@
+package test
+
 fun main() {
     val studentA = Student("安学民",18, "male")
 
@@ -6,6 +8,11 @@ fun main() {
     studentA.sayHello(petParse)
 
     println("i like ${studentA.hobby()}")
+
+    val myTest = MyTest(1)
+    println(myTest.a)
+    myTest.a = 2
+    println(myTest.a)
 }
 
 open class Person(val name: String, var age: Int){
@@ -29,3 +36,5 @@ class Student(name: String, age: Int, val gender: String) : Person(name, age) {
 
     override fun hobby() :String = "football"
 }
+
+data class MyTest(var a :Int)
---
+++ src/test/kotlin/calculator/TestCalculatorV3.kt
@@ -0,0 +1,16 @@
+package calculator
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class TestCalculatorV3 {
+
+    @Test
+    fun testCalculate() {
+        val calculator = CalculatorV3()
+
+        val res1 = calculator.calculate("2333333333333332+1")
+        assertEquals("2333333333333333", res1)
+        println("done")
+    }
+}
